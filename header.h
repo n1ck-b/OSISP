a90f5a4fb76d139cb787cc5576175043b5f5d7f1
Nikita <inbolotin@gmail.com>
2025-03-22 23:14:07 +0300

    feat:lab3 version 1

---
+++ header.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <locale.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <signal.h>
